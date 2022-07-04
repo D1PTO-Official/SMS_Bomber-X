aef96b50c17f3557ba816d458ca3685330219de0
IT"Z DIPTO VAU OKAY <107912629+D1PTO-Official@users.noreply.github.com>
2022-07-05 03:12:47 +0600

    Create Logo2.py

---
+++ Logo2.py
@@ -0,0 +1,62 @@
+import os
+import sys
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+purple="\033[0;35m"       # Purple
+os.system("clear")
+
+black="\033[0;30m"        # Black
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+yellow="\033[0;33m"       # Yellow
+blue="\033[0;34m"         # Blue
+purple="\033[0;35m"       # Purple
+cyan="\033[0;36m"         # Cyan
+white="\033[0;37m"        # White
+
+print(green+"[✓] Wellcome To My World ")
+
+a="Name      : IT'Z DIPTO VAU 😴✌️"
+b="Status    : FUCK YOUR LIFE 👅💦"
+c="Facebook  : https://www.facebook.com/ITZ.DIPTO.TFG.KING.OKAY"
+
+logo=red+str("""
+d8888b. d888888b d8888b. d888888b  .d88b.  
+88  `8D   `88'   88  `8D `~~88~~' .8P  Y8. 
+88   88    88    88oodD'    88    88    88 
+88   88    88    88~~~      88    88    88 
+88  .8D   .88.   88         88    `8b  d8' 
+Y8888D' Y888888P 88         YP     `Y88P'
+""")  
+print(green+"-----------------------------------------------------------")                                                                                      
+print(logo)
+print(cyan+"-----------------------------------------------------------")
+print(green+a)
+print(purple+"-----------------------------------------------------------")
+print(b)
+print(yellow+"-----------------------------------------------------------")
+print(c)
+print(green+"-----------------------------------------------------------")
+red="\033[0;31m"          # Red
+green="\033[0;32m"        # Green
+purple="\033[0;35m"       # Purple
+
+us=str(input(green+"Enter Your Username :"))
+pas=str(input(purple+"Enter Your Password :"))
+
+fuck="D1PTO V4U"
+sex="FUCKER"
+
+if us==fuck and pas==sex:
+	os.system("clear")
+	print(green+"[✓] Your Programming Enter Sucses-!!")
+	pass
+else:
+	print(red+"[x] Wrong Password-!")
+	sys.exit()
+	
+print(red+"------------------------------------------------------------")
+print(green+"Remember This Line : IT'Z D1PTO V4U FUCK YOUR LIFE👅💦🖕")
+print(red+"____________________________________________________________")
+print(purple+"V4U Ar Attitude    : Fuck Your System Okay Bbz -!💋😋💦")
+print(red+"____________________________________________________________")
